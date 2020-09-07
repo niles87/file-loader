1cea6c38bb15e3a2cc9369184a16657bd243dfbd
Niles87 <nbing19@gmail.com>
2020-09-06 17:58:40 -0700

    update

--- server/api.js
+++ server/api.js
@@ -28,6 +28,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("*", (req, res) => {
+  console.log(path.join(__dirname, "..", "client", "build", "index.html"));
   res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
 
